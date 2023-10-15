6881c9e8c7108b037139461acb24fce57dfa6b69
Vohiro <dvohiro@gmail.com>
2023-10-14 17:23:23 -0700

    task 7 console

--- console.py
+++ console.py
@@ -4,22 +4,13 @@ import cmd
 from models.base_model import BaseModel
 import json
 from models import storage
-
+import re
 
 class HBNBCommand(cmd.Cmd):
 
     """The class def for command interpreter"""
 
     prompt = "(hbnb) "
-    __classes = {
-        "BaseModel",
-        "User",
-        "State",
-        "City",
-        "Place",
-        "Amenity",
-        "Review"
-    }
 
     def do_quit(self, line):
         """Quit command to exit the program"""
@@ -48,7 +39,7 @@ class HBNBCommand(cmd.Cmd):
 
         if line == "" or line is None:
             print("** class name missing **")
-        elif line not in storage.classes:
+        elif line not in storage.classes():
             print("** class doesn't exist **")
         else:
             objdict = storage.classes()[line]()
@@ -67,7 +58,7 @@ class HBNBCommand(cmd.Cmd):
             elif len(line_list) < 2:
                 print("** instance id missing **")
             else:
-                set_obj = "{}.{}".format(words[0], words[1])
+                set_obj = "{}.{}".format(line_list[0], line_list[1])
                 if set_obj not in storage.all():
                     print("** no instance found **")
                 else:
@@ -85,12 +76,74 @@ class HBNBCommand(cmd.Cmd):
             elif len(line_list) < 2:
                 print("** instance id missing **")
             else:
-                set_obj = "{}.{}".format(words[0], words[1])
+                set_obj = "{}.{}".format(line_list[0], line_list[1])
                 if set_obj not in storage.all():
                     print("** no instance found **")
                 else:
                     del storage.all()[key]
                     storage.save()
+
+    def do_all(self, line):
+        """Prints all string representation of all instances """
+
+        if line != "":
+            line_list = line.split(' ')
+            if line_list[0] not in storage.classes():
+                print("** class doesn't exist **")
+            else:
+                new_lt = [str(obj) for key, obj in storage.all().items()
+                      if obj.__class__.__name__ == line_list[0]]
+                print(new_lt)
+        else:
+            new_list = [str(obj) for key, obj in storage.all().items()]
+            print(new_list)
+
+    def do_update(self, line):
+        """Updates an instance by adding or updating attribute """
+
+        if line == "" or line is None:
+            print("** class name missing **")
+            return
+
+        rex = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
+        match = re.search(rex, line)
+        classname = match.group(1)
+        uid = match.group(2)
+        attribute = match.group(3)
+        value = match.group(4)
+        if not match:
+            print("** class name missing **")
+        elif classname not in storage.classes():
+            print("** class doesn't exist **")
+        elif uid is None:
+            print("** instance id missing **")
+        else:
+            set_obj = "{}.{}".format(classname, uid)
+            if set_obj not in storage.all():
+                print("** no instance found **")
+            elif not attribute:
+                print("** attribute name missing **")
+            elif not value:
+                print("** value missing **")
+            else:
+                cast = None
+                if not re.search('^".*"$', value):
+                    if '.' in value:
+                        cast = float
+                    else:
+                        cast = int
+                else:
+                    value = value.replace('"', '')
+                attributes = storage.attributes()[classname]
+                if attribute in attributes:
+                    value = attributes[attribute](value)
+                elif cast:
+                    try:
+                        value = cast(value)
+                    except ValueError:
+                        pass  # fine, stay a string then
+                setattr(storage.all()[set_obj], attribute, value)
+                storage.all()[set_obj].save()
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,12 +2,12 @@
 """Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
-#from models.user import User
-#from models.state import State
-#from models.city import City
-#from models.place import Place
-#from models.amenity import Amenity
-#from models.review import Review
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -47,3 +47,55 @@ class FileStorage:
                     self.new(eval(clas_name)(**obj))
         except FileNotFoundError:
             return
+    
+    def attributes(self):
+        """Returns the valid attributes and their types for classname"""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
+
